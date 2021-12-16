c3c0b95f1a0b8d7dd1c6ad6c503e43c938c71564
Barbaro Javier <bjvalmaseda.g@gmail.com>
2021-12-16 11:44:32 -0500

    feat(todos): ✨ Create Todo Model
    Create Model, Controller, Factory and Seeder for Todo

---
+++ src/database/migrations/2021_12_16_163800_create_todos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("todos", function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table
+                ->foreignId("user_id")
+                ->constrained("users")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("todos");
+    }
+}
