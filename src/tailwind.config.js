baff56a192095f19ef962d483f3e528c18349262
Barbaro Javier <bjvalmaseda.g@gmail.com>
2021-12-17 11:55:47 -0500

    build(ui): ⚡️ Change Tailwind breakpoints

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
-        extend: {},
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1230px",
+            "2xl": "1536px",
+        },
     },
     plugins: [],
 };
