2bb62301f9c7d690a25cbc18bc3c3e51a045c255
Barbaro Javier <bjvalmaseda.g@gmail.com>
2021-12-19 10:40:46 -0500

    test(todos): ✅ E2E test create todo past

--- src/tests/Browser/TodoTest.php
+++ src/tests/Browser/TodoTest.php
@@ -10,16 +10,17 @@ use Tests\DuskTestCase;
 class TodoTest extends DuskTestCase
 {
     use DatabaseMigrations;
-    public function testCreateTodo()
+    public function test_create_todo()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit("/")->assertSee("Type to add new task");
             $browser->click("#new")->assertSee("Open");
             $browser->type("newTitle", "Some text");
-            $browser->pause(2000);
-            $browser->click(".primary-button");
-
-            $this->assertEquals(1, Todo::count());
+            $browser->pause(1500);
+            $browser
+                ->click(".primary-button")
+                ->pause(500)
+                ->assertSee("Some text");
         });
     }
 }
